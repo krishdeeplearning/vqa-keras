9fe6e808b1f721bde9dedf10573e0d15ac190619
krishvs <krishnanvenkat@krishnans-MacBook-Pro.local>
2016-10-31 11:36:44 -0400

    corrected file unzip command

--- setup_data.py
+++ setup_data.py
@@ -2,6 +2,6 @@ import os
 
 def download():
 	os.system('wget https://www.dropbox.com/s/l2wzvpl7dws74j7/data.zip?dl=1')
-	os.system('unzip data.zip')
+	os.system('unzip data.zip?dl=1')
 
 download()
